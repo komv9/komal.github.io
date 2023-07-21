9fce29fd721e719341b48a7ac3229327ec2be972
Komal Kumari <komvishwa9@gmail.com>
2023-07-21 22:31:41 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Komal: Portfolio</title>
+</head>
+<body>
+Hello Komal
+</body>
+</html>
